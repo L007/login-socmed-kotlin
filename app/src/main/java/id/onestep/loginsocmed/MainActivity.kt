dae4173f711347d4c1c2d6f233db60592844de14
L007 <antoeldi@gmail.com>
2019-02-06 17:41:35 +0700

    add Login FB

--- app/src/main/java/id/onestep/loginsocmed/MainActivity.kt
+++ app/src/main/java/id/onestep/loginsocmed/MainActivity.kt
@@ -5,53 +5,114 @@ import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
 import android.util.Log
 import android.widget.Toast
+import com.facebook.*
+import com.facebook.login.LoginManager
+import com.facebook.login.LoginResult
 import com.google.android.gms.auth.api.Auth
 import com.google.android.gms.auth.api.signin.*
 import kotlinx.android.synthetic.main.activity_main.*
+import org.json.JSONObject
+import java.lang.Exception
+import java.util.*
 
 class MainActivity : AppCompatActivity() {
-    val RC_SIGN_IN:Int = 1
+    val RC_SIGN_IN: Int = 1
+    private var callbackManager: CallbackManager? = null
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
         setupGoogle()
+        setupFb()
+
+
     }
 
     override fun onActivityResult(requestCode: Int, resultCode: Int, data: Intent?) {
         super.onActivityResult(requestCode, resultCode, data)
+        callbackManager?.onActivityResult(requestCode, resultCode, data) //penting
 
-        if(requestCode==RC_SIGN_IN){
-            val result : GoogleSignInResult = Auth.GoogleSignInApi.getSignInResultFromIntent(data)
+        if (requestCode == RC_SIGN_IN) {
+            val result: GoogleSignInResult = Auth.GoogleSignInApi.getSignInResultFromIntent(data)
             handleSignResult(result)
         }
 
     }
 
     private fun handleSignResult(result: GoogleSignInResult) {
-        if (result.isSuccess){
-            val account : GoogleSignInAccount? = result.signInAccount
+        if (result.isSuccess) {
+            val account: GoogleSignInAccount? = result.signInAccount
             Log.d("account : ", "nama : ${account?.displayName} email : ${account?.email}")
-            val intent : Intent = Intent(this,ProfileActivity::class.java)
-            intent.putExtra("account",account)
+            val intent: Intent = Intent(this, ProfileActivity::class.java)
+            intent.putExtra("datagoogle", account)
             startActivity(intent)
-        }
-        else{
-            Toast.makeText(this,"Login Gagal",Toast.LENGTH_SHORT)
+        } else {
+            Toast.makeText(this, "Login Gagal", Toast.LENGTH_SHORT)
         }
     }
 
-    fun setupGoogle(){
-        val gso:GoogleSignInOptions = GoogleSignInOptions.Builder(GoogleSignInOptions.DEFAULT_SIGN_IN)
+    fun setupGoogle() {
+        val gso: GoogleSignInOptions = GoogleSignInOptions.Builder(GoogleSignInOptions.DEFAULT_SIGN_IN)
             .requestEmail()
             .build()
-        val gsoClient:GoogleSignInClient = GoogleSignIn.getClient(this,gso)
+        val gsoClient: GoogleSignInClient = GoogleSignIn.getClient(this, gso)
 
         btn_sign_google.setOnClickListener {
-            val signIntent : Intent = gsoClient.signInIntent
-            startActivityForResult(signIntent,RC_SIGN_IN)
+            val signIntent: Intent = gsoClient.signInIntent
+            startActivityForResult(signIntent, RC_SIGN_IN)
 
         }
+    }
+
+    fun setupFb() {
+        callbackManager = CallbackManager.Factory.create()
+        LoginManager.getInstance().registerCallback(callbackManager, object : FacebookCallback<LoginResult> {
+            override fun onSuccess(result: LoginResult?) {
+                //Log.d("response FB : ",result?.accessToken?.token)
+                fbGraphReq(result?.accessToken!!)
+
+
+            }
+
+            override fun onCancel() {
+                TODO("not implemented") //To change body of created functions use File | Settings | File Templates.
+            }
+
+            override fun onError(error: FacebookException?) {
+                TODO("not implemented") //To change body of created functions use File | Settings | File Templates.
+            }
+
+        })
+
+
+
+    }
+
+    fun fbGraphReq(accessToken: AccessToken) {
+        val request: GraphRequest =
+            GraphRequest.newMeRequest(accessToken, object : GraphRequest.GraphJSONObjectCallback {
+                override fun onCompleted(`object`: JSONObject?, response: GraphResponse?) {
+                    val intent = Intent(applicationContext, ProfileActivity::class.java)
+                    if (response != null) {
+                        try {
+                            val data: JSONObject? = response?.jsonObject
+                            //Log.d("response object ", data.toString())
+                            intent.putExtra("datafb",data?.toString())
+                            startActivity(intent)
+                        } catch (e: Exception) {
+                            Log.d("pesan error", e.message)
+                        }
+                    }
+
+
+                }
+
+            })
+
+        val param = Bundle()
+        param.putString("fields", "id,name,email");
+        request.parameters = param
+        request.executeAsync()
     }
 }
--- app/src/main/java/id/onestep/loginsocmed/ProfileActivity.kt
+++ app/src/main/java/id/onestep/loginsocmed/ProfileActivity.kt
@@ -3,47 +3,107 @@ package id.onestep.loginsocmed
 import android.content.Intent
 import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
+import android.util.Log
 import com.bumptech.glide.Glide
 import com.bumptech.glide.request.RequestOptions
+import com.facebook.AccessToken
+import com.facebook.GraphRequest
+import com.facebook.HttpMethod
+import com.facebook.login.LoginManager
 import com.google.android.gms.auth.api.signin.GoogleSignIn
 import com.google.android.gms.auth.api.signin.GoogleSignInAccount
 import com.google.android.gms.auth.api.signin.GoogleSignInClient
 import com.google.android.gms.auth.api.signin.GoogleSignInOptions
 import kotlinx.android.synthetic.main.activity_profile.*
+import org.json.JSONObject
 
 class ProfileActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_profile)
-        getData()
+
+
+        if (intent.getStringExtra("datafb") != null) {
+
+            getDataFb()
+        } else {
+            getData()
+
+        }
 
         btn_signOut.setOnClickListener {
             signOut()
         }
     }
 
-    fun getData(){
-        val account : GoogleSignInAccount? = intent.getParcelableExtra("account")
+    fun getData() {
+        //var dataFb: JSONObject? = JSONObject(intent.getStringExtra("datafb"))
+        var dataGoogle: GoogleSignInAccount? = intent.getParcelableExtra("datagoogle")
 
-        Glide.with(this)
-            .load(account?.photoUrl)
-            .apply(RequestOptions().circleCrop())
-            .into(img_profile)
+        //Log.d("data fb", dataFb.toString())
+        Log.d("data google", dataGoogle.toString())
 
-        txt_username.setText("Welcome Back, ${account?.displayName}")
+        if (dataGoogle != null) {
+
+            Glide.with(this)
+                .load(dataGoogle?.photoUrl)
+                .apply(RequestOptions().circleCrop())
+                .into(img_profile)
+
+            txt_username.setText("Welcome Back, ${dataGoogle?.displayName}")
+        } /*else if (dataFb != null) {
+            Glide.with(this)
+                .load("https://graph.facebook.com/${dataFb?.getString("id")}/picture?type=large")
+                .apply(RequestOptions().circleCrop())
+                .into(img_profile)
+
+            txt_username.setText("Welcome Back, ${dataFb?.getString("name")}")
+        }*/
+
     }
 
-    fun signOut(){
+    fun getDataFb() {
+        var dataFb: JSONObject? = JSONObject(intent.getStringExtra("datafb"))
+
+
+        Log.d("data fb", dataFb.toString())
+
+        if (dataFb != null) {
+            Glide.with(this)
+                .load("https://graph.facebook.com/${dataFb?.getString("id")}/picture?type=large")
+                .apply(RequestOptions().circleCrop())
+                .into(img_profile)
+
+            txt_username.setText("Welcome Back, ${dataFb?.getString("name")}")
+        }
+
+    }
+
+    fun signOut() {
         val gso: GoogleSignInOptions = GoogleSignInOptions.Builder(GoogleSignInOptions.DEFAULT_SIGN_IN)
             .requestEmail()
             .build()
-        val gsoClient: GoogleSignInClient = GoogleSignIn.getClient(this,gso)
+        val gsoClient: GoogleSignInClient = GoogleSignIn.getClient(this, gso)
         gsoClient.signOut()
             .addOnCompleteListener(this) {
-                val intent = Intent(this,MainActivity::class.java)
+                val intent = Intent(this, MainActivity::class.java)
                 startActivity(intent)
             }
+
+        if (AccessToken.getCurrentAccessToken() != null) {
+            GraphRequest(
+                AccessToken.getCurrentAccessToken(),
+                "/me/permissions/",
+                null,
+                HttpMethod.DELETE,
+                GraphRequest.Callback {
+                    AccessToken.setCurrentAccessToken(null)
+                    LoginManager.getInstance().logOut()
+                    finish()
+                }).executeAsync()
+        }
+
 
     }
 }
